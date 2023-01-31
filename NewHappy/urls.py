f4e1bab13064f63cea93c0f1eefcc9161a190691
BOWEN ZHU <bowen.zhu@kcl.ac.uk>
2023-01-31 20:13:58 +0000

    initialize user feed page and seed and unseed file

--- NewHappy/urls.py
+++ NewHappy/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from pst import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('userFeed/', views.userFeed, name = 'userFeed')
 ]
