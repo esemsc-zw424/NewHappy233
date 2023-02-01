0041182d38da95cd699532c392605cfa98935a4a
BOWEN ZHU <bowen.zhu@kcl.ac.uk>
2023-02-01 17:25:19 +0000

    create spending model and relevant tests for spending model

--- pst/models.py
+++ pst/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
+from django.core.validators import RegexValidator,MaxValueValidator,MinValueValidator
 
 # Create your models here.
+class Spending_type(models.TextChoices):
+    Expenditure = 'Expenditure'
+    Income = 'Income'
+
+
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True, blank=False)
+    username = models.CharField(max_length=50, unique=True)
+    
+    def __str__(self):
+        return self.username
+
+class Spending(models.Model):
+    spending_owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='spendingOwner', blank = False) #this refers to the user when create this spending
+
+    amount = models.IntegerField( # this refers to the amount this user spent or gained
+        blank=False,
+        validators=[
+            MaxValueValidator(10000000),
+            MinValueValidator(0),
+        ]
+    )
+
+    descriptions = models.CharField( # comments for the spending
+        blank = True,
+        max_length=500,
+    )
+
+    date = models.DateField( # data of the spending
+        blank = False,
+    )
+
+    spending_type = models.CharField( # this refers to the spending type 
+        max_length=30,
+        choices=Spending_type.choices,
+        default=Spending_type.Expenditure,
+        blank = False,
+    )
+
+    # spending_category = models.ForeignKey(Categories, on_delete=models.CASCADE) #this refers to the category of the spending
+
+
---
+++ pst/tests/models/test_spending_model.py
@@ -0,0 +1,110 @@
+from django.core.exceptions import ValidationError
+from django.test import TestCase
+from pst.models import Spending, Spending_type, User
+
+class SpendingModelTestCase(TestCase):
+
+    # this test will test all field in Spending model
+    fixtures = ['pst/tests/fixtures/users.json'], ['pst/tests/fixtures/spending.json']
+
+    def setUp(self):
+        self.spending_owner = User.objects.get(email = "johndoe@example.org")
+
+        self.spending = Spending.objects.get(descriptions = "This is test spending 1")
+
+    def _assert_spending_is_valid(self):
+        try:
+            self.spending.full_clean()
+        except(ValidationError):
+            self.fail('Test spending should be valid')
+
+    def _assert_spending_is_invalid(self):
+        with self.assertRaises(ValidationError):
+            self.spending.full_clean()
+
+    def test_valid_spending(self):
+        self._assert_spending_is_valid()
+
+
+    def test_spending_owner_cannot_be_blank(self):
+        self.spending.spending_owner = None
+        self._assert_spending_is_invalid()
+
+
+    def test_spending_amount_cannot_be_blank(self):
+        self.spending.amount = ''
+        self._assert_spending_is_invalid()
+
+    def test_spending_amount_cannot_be_larger_than_10000000(self):
+        self.spending.amount = 10000001
+        self._assert_spending_is_invalid()
+
+    def test_spending_amount_can_be_10000000(self):
+        self.spending.amount = 10000000
+        self._assert_spending_is_valid()
+
+    def test_spending_amount_can_be_0(self):
+        self.spending.amount = 0
+        self._assert_spending_is_valid()
+
+    def test_spending_amount_cannot_be_less_than_0(self):
+        self.spending.amount = -1
+        self._assert_spending_is_invalid()
+
+    def test_spending_amount_must_not_be_unique(self):
+        second_spending = Spending.objects.get(descriptions = "This is test spending 2")
+        self.spending.amount = second_spending.amount
+        self._assert_spending_is_valid()
+
+    def test_spending_amount_must_only_contain_numbers(self):
+        self.spending.amount = 'xxx'
+        self._assert_spending_is_invalid()
+
+
+    def test_amount_must_only_contain_number(self):
+        self.spending.amount = 'sss'
+        self._assert_spending_is_invalid()
+
+    def test_spending_description_can_be_blank(self):
+        self.spending.descriptions = ''
+        self._assert_spending_is_valid()
+
+    def test_spending_descriptions_can_be_500_characters_long(self):
+        self.spending.descriptions = 'x' * 500
+        self._assert_spending_is_valid()
+
+    def test_spending_descriptions_cannot_be_500_characters_long(self):
+        self.spending.descriptions = 'x' * 501
+        self._assert_spending_is_invalid()
+
+    def test_spending_descriptions_must_not_be_unique(self):
+        second_spending = Spending.objects.get(descriptions = "This is test spending 2")
+        self.spending.descriptions = second_spending.descriptions
+        self._assert_spending_is_valid()
+
+    
+    def test_date_cannot_be_blank(self):
+        self.spending.date = None
+        self._assert_spending_is_invalid()
+
+    def test_spending_type_cannot_be_blank(self):
+        self.spending.spending_type = ''
+        self._assert_spending_is_invalid()
+
+    def test_spending_type_can_only_be_one_of_the_choice_Expenditure(self):
+        self.spending.spending_type = 'Expenditure'
+        self._assert_spending_is_valid()
+
+    def test_spending_type_can_only_be_one_of_the_choice_Income(self):
+        self.spending.spending_type = 'Income'
+        self._assert_spending_is_valid()
+
+    def test_spending_type_can_only_be_one_of_the_choice_other_text(self):
+        self.spending.spending_type = 'xxxx'
+        self._assert_spending_is_invalid()
+
+    def test_spending_type_must_not_be_uniqure(self):
+        second_spending = Spending.objects.get(descriptions = "This is test spending 2")
+        self.spending.spending_type = second_spending.spending_type
+        self._assert_spending_is_valid()
+        
