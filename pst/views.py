eaa70ec2f6b749a3553d945f00ab7c73e14fae57
Runxin Shao <runxin.shao@kcl.ac.uk>
2023-02-01 21:59:26 +0000

    create a visitor_signup html

--- pst/views.py
+++ pst/views.py
@@ -1,6 +1,9 @@
 from django.contrib.auth.decorators import login_required
+from django.contrib import auth
 from django.shortcuts import render,redirect
 from django.contrib import messages
+from django.forms import VisitorSignupForm
+
 
 # Create your views here.
 
@@ -8,3 +11,16 @@ from django.contrib import messages
 def user_feed(request):
     return render(request, 'user_feed.html')
 
+
+def visitor_signup(request):
+    if request.method == 'POST':
+        form = VisitorSignupForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth.login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'signup.html', {'form': form})
+    else:
+        form = VisitorSignupForm()
+        return render(request, 'signup.html', {'form': form})
