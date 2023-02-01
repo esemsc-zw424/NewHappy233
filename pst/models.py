91cf5476299686eadbe2145a8f57ba5ac1c31dbb
Jiale Yang <jiale.2.yang@kcl.ac.uk>
2023-02-01 19:59:11 +0000

    modify spending model

--- pst/models.py
+++ pst/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.core.validators import MinValueValidator
+
 
 # Create your models here.
+class Spending(models.Model):
+    title = models.CharField(max_length=20, blank=False)
+    amount = models.IntegerField(blank=False, validators=[MinValueValidator(0)])
+    #category = models.ForeignKey(Category, blank=False, on_delete=models.CASCADE)
+    description = models.TextField(max_length=100, blank=True)
+    file = models.FileField(null=True, blank=True) 
+    image = models.ImageField(null=True, blank=True)
+
+    
