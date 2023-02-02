9cf47c1640b8877c765f2d8633ca389bb73264c0
TengyuSun <tengyu.sun@kcl.ac.uk>
2023-02-02 19:39:13 +0000

    Implement set budget fuction

--- pst/models.py
+++ pst/models.py
@@ -70,4 +70,10 @@ class Spending(models.Model):
 
     # spending_category = models.ForeignKey(Categories, on_delete=models.CASCADE) #this refers to the category of the spending
 
-
+class Budget(models.Model):
+    name = models.CharField(max_length=100)
+    limit = models.DecimalField(max_digits=10, decimal_places=2)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
