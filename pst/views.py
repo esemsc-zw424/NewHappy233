584bcee44a6eccaf94862ba6a6a2a1ee6b5b8ba9
BOWEN ZHU <bowen.zhu@kcl.ac.uk>
2023-02-04 00:06:39 +0000

    create a simple version of chatbot

--- pst/views.py
+++ pst/views.py
@@ -4,6 +4,13 @@ from django.shortcuts import render,redirect
 from django.contrib import messages
 from pst.forms import VisitorSignupForm
 from pst.helpers.auth import login_prohibited
+from django.http import HttpResponse
+import random
+import nltk
+nltk.download('punkt')
+nltk.download('wordnet')
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
 
 # Create your views here.
 
@@ -32,3 +39,38 @@ def home(request):
 
 def visitor_introduction(request):
     return render(request, 'visitor_introduction.html')
+
+#Chatbot is a simple virtual help assistant that can answer user's question base on keywords
+def chat_bot(request):
+    chat_history = [] # this is use to store all the chat history between user and chatbot
+    if request.method == 'POST':
+        user_input = request.POST['user_input']
+        chat_bot_response = respond(user_input)
+        chat_history.append((user_input, chat_bot_response))
+        return render(request, 'chat_bot.html', {'chat_history': chat_history})
+    return render(request, 'chat_bot.html', {'chat_history': chat_history})
+
+def respond(user_input):
+    lemmatizer = WordNetLemmatizer()
+    keywords = {
+        "hello": ["hi", "hello", "hey", "greetings", "heya", "hola", "what's up", "sup"],
+        "bye": ["bye", "goodbye", "see you later", "adios", "later", "farewell"]
+    }
+
+    responses = {
+        "hello": ["Hello! How may I help you?", "Hi there! How can I assist you today?"],
+        "bye": ["Goodbye! Have a great day!", "See you later! Take care."]
+    }
+
+    tokens = word_tokenize(user_input)
+    tokens = [lemmatizer.lemmatize(token.lower()) for token in tokens]
+
+    for keyword, synonyms in keywords.items():
+        for token in tokens:
+            if token in synonyms:
+                return random.choice(responses[keyword])
+    return "Sorry, I do not understand what you mean."
+
+
+
+
