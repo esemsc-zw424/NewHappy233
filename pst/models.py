2a8055cf66676848ca49ab426ddda868322ffdca
TengyuSun <tengyu.sun@kcl.ac.uk>
2023-02-06 06:01:07 +0800

    "Fix bugs when saving budget"

--- pst/models.py
+++ pst/models.py
@@ -73,7 +73,7 @@ class Spending(models.Model):
 class Budget(models.Model):
     name = models.CharField(max_length=100)
     limit = models.DecimalField(max_digits=10, decimal_places=2)
-    start_date = models.DateField()
-    end_date = models.DateField()
+    # start_date = models.DateField()
+    # end_date = models.DateField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
