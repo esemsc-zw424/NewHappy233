5e0d4ad2e993388b635a888a2d3215c645961159
Runxin Shao <runxin.shao@kcl.ac.uk>
2023-03-12 00:02:44 +0000

    put js from home.html to static and add icon for points

---
+++ static/javascript/daily_reward.js
@@ -0,0 +1,26 @@
+
+
+$(document).ready (function() {
+
+    var modal = document.getElementById('reward');
+    var modalInstance = new bootstrap.Modal(modal);
+    modalInstance.show();
+    });
+    
+    
+    const rewardPos = document.getElementById("rewardPos");
+    
+    
+function rewardPosClickHandler() {
+const xhr = new XMLHttpRequest();
+xhr.open('GET', 'get_reward_points/');
+xhr.onload = function() {
+    if (xhr.status === 200) {
+    rewardPos.innerHTML = "<i class='bi bi-emoji-smile'></i>congrats";     
+    } else {
+    console.log('Request failed.  Returned status of ' + xhr.status);
+    }
+};
+xhr.send();
+
+}
