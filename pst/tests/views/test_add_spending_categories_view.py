21e73edc33f3823f3277b86e99f42fed3f992131
BOWEN ZHU <bowen.zhu@kcl.ac.uk>
2023-03-17 19:44:50 +0000

    finish test for add_spending_Categories

--- pst/tests/views/test_add_spending_categories_view.py
+++ pst/tests/views/test_add_spending_categories_view.py
@@ -12,6 +12,10 @@ class AddSpendingCategoriesTestCase(TestCase):
         self.url = reverse('add_spending_categories')
         self.categories = Categories.objects.get(name = 'Food')
         self.user = User.objects.get(email = "johndoe@example.org")
+        self.valid_data = {
+            'name': 'Groceries',
+            'categories_type': 'Expenditure'
+        }
 
     def test_add_categories_url(self):
         self.assertEqual(self.url, '/add_spending_categories/')
@@ -25,6 +29,42 @@ class AddSpendingCategoriesTestCase(TestCase):
         self.assertIsInstance(form, CategoriesForm)
         self.assertFalse(form.is_bound)
 
+    def test_add_spending_categories_view_Expenditure(self):
+        self.client.login(username=self.user.email, password="Password123")
+
+        spending_categories_count_before = Categories.objects.count()
+
+        response = self.client.post(self.url, self.valid_data, follow=True)
+        self.assertEqual(response.status_code, 200)
+        messages = list(response.context.get('messages'))
+        self.assertEqual(len(messages), 1)
+        self.assertEqual(str(messages[0]), 'Category successfully added')
+        spending_categories_count_after = Categories.objects.count()
+        self.assertEqual(spending_categories_count_after, spending_categories_count_before + 1)
+
+        category = Categories.objects.last()
+        self.assertEqual(category.owner, self.user)
+        self.assertEqual(category.categories_type, 'Expenditure')
+
+    def test_add_spending_categories_view_Income(self):
+        self.client.login(username=self.user.email, password="Password123")
+
+        self.valid_data['categories_type'] = 'Income'
+
+        spending_categories_count_before = Categories.objects.count()
+
+        response = self.client.post(self.url, self.valid_data, follow=True)
+        self.assertEqual(response.status_code, 200)
+        messages = list(response.context.get('messages'))
+        self.assertEqual(len(messages), 1)
+        self.assertEqual(str(messages[0]), 'Category successfully added')
+        spending_categories_count_after = Categories.objects.count()
+        self.assertEqual(spending_categories_count_after, spending_categories_count_before + 1)
+
+        category = Categories.objects.last()
+        self.assertEqual(category.owner, self.user)
+        self.assertEqual(category.categories_type, 'Income')
+            
     # def test_access_add_categories_when_not_log_in(self):
     #     response = self.client.get(self.url)
     #     self.assertEqual(response.status_code, 302)
