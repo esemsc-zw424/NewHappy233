4f79b6fbb1938d8ffac8d69f9fe2cf002daac5ee
Runxin Shao <runxin.shao@kcl.ac.uk>
2023-03-18 23:59:02 +0000

    add a base_home.html to store the config for home page

--- NewHappy/settings.py
+++ NewHappy/settings.py
@@ -170,6 +170,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static/img'),
 ]
 
 # Default primary key field type
