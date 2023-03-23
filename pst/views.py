1842f41f423f23a343326bf6659e6037209a73ac
Runxin Shao <runxin.shao@kcl.ac.uk>
2023-03-23 01:46:14 +0000

    put mixin to utils.py

--- pst/views.py
+++ pst/views.py
@@ -38,6 +38,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.hashers import check_password
 from django.core.exceptions import ImproperlyConfigured, ObjectDoesNotExist
+from pst.utils import LoginProhibitedMixin
 import os
 from django.db.models.signals import pre_delete
 from django.dispatch import receiver
@@ -59,45 +60,8 @@ def user_feed(request):
     return render(request, 'user_feed.html')
 
 
-# @login_prohibited
-# def visitor_signup(request):
-#     if request.method == 'POST':
-#         form = VisitorSignupForm(request.POST)
-#         if form.is_valid():
-#             user = form.save()
-#             auth.login(request, user, backend='django.contrib.auth.backends.ModelBackend')
-#             return redirect('home')
-#         else:
-#             return render(request, 'visitor_signup.html', {'form': form})
-#     else:
-#         form = VisitorSignupForm()
-#         return render(request, 'visitor_signup.html', {'form': form})
-
-class LoginProhibitedMixin:
 
 
-    redirect_when_logged_in_url = None
-
-    def dispatch(self, *args, **kwargs):
-        """Redirect when logged in, or dispatch as normal otherwise."""
-        if self.request.user.is_authenticated:
-            return self.handle_already_logged_in(*args, **kwargs)
-        return super().dispatch(*args, **kwargs)
-
-    def handle_already_logged_in(self, *args, **kwargs):
-        url = self.get_redirect_when_logged_in_url()
-        return redirect(url)
-
-    def get_redirect_when_logged_in_url(self):
-        """Returns the url to redirect to when not logged in."""
-        if self.redirect_when_logged_in_url is None:
-            raise ImproperlyConfigured(
-                "LoginProhibitedMixin requires either a value for "
-                "'redirect_when_logged_in_url', or an implementation for "
-                "'get_redirect_when_logged_in_url()'."
-            )
-        else:
-            return self.redirect_when_logged_in_url
 
 
 class SignUpView(LoginProhibitedMixin, FormView):
